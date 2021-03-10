83295b74daab1909c440d954c417c573f734f8ef
Vijay Krishnamoorthy <64549719+vijayk0747@users.noreply.github.com>
2021-03-09 21:09:54 -0600

    Update JSBot.js

--- JSBot.js
+++ JSBot.js
@@ -12,6 +12,7 @@ JSBotClient.on("message", (message) => {
         message.channel.send('Vijay')
     }
 
+    //Added weather functionality using openweathermap API
     if (message.content.includes("!weather") && message.author.bot === false) {
         let zipCode = message.content.split(" ")[1];
         if (zipCode === undefined || zipCode.length != 5 || parseInt(zipCode) === NaN) {
@@ -19,28 +20,52 @@ JSBotClient.on("message", (message) => {
             .catch(console.error)
             return;
         } else {
-            fetch(`http://api.openweathermap.org/data/2.5/weather?zip=${zipCode},US&appid=`)
+            fetch(`http://api.openweathermap.org/data/2.5/weather?zip=${zipCode},US&appid=`) //APIKEY is hidden.
                 .then(response => {
                     return response.json()
                 })
-                .then(parsedWeather => {
-                    if (parsedWeather.cod === '404') {
+                .then(data => {
+                    if (data.cod === '404') {
                         message.channel.send("`This zip code does not exist.`")
                     } else {
                             const embed = new Discord.MessageEmbed()
-                            .setColor('#7FFFD4')
+                            .setColor('#F5C9B9')
                             .setTitle('Weather Report')
-                            .addField('Location: ', `${parsedWeather.name}, ${parsedWeather.sys.country} `)
-                            .addField('Forecast: ', `${parsedWeather.weather[0].main}`)
-                            .addField('Current Temperature: ', `${(Math.round(((parsedWeather.main.temp - 273.15) * 9/5 +32)))} F`)
+                            .addField('Location: ', `${data.name}, ${data.sys.country} `)
+                            .addField('Forecast: ', `${data.weather[0].main}`)
+                            .addField('Current Temperature: ', `${(Math.round(((data.main.temp - 273.15) * 9/5 +32)))} F`)
                             
                             return message.channel.send(embed);
                     }
                 })
         }
     }
+
+    //Added F1 Data Functionality using ergast API
+    if (message.content.includes("!F1") && message.author.bot === false)
+    {
+        let fSeason = message.content.split(" ")[1];
+        if (fSeason === undefined || fSeason.length != 4 || parseInt(fSeason) === NaN) {
+            message.channel.send("`Invalid Season`")
+            .catch(console.error)
+            return;
+        } else {
+            fetch(`http://ergast.com/api/f1/${fSeason}/driverStandings.json`)
+            .then(response => {
+                return response.json()
+            })
+            .then(data => {
+                const embed = new Discord.MessageEmbed()
+                .setColor('#000000')
+                .setTitle('F1')
+                .addField('Standings: ', `${data.MRData.StandingsTable.StandingsList.DriverStanding.Driver.GivenName}`)
+
+                return message.channel.send(embed);
+            })
+        }
+    }
 })
 
-//Bot Token and API Token Hidden for Safety Reasons
+//API Token Hidden
 JSBotClient.login('')
 
